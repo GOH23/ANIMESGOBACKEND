34b83900343c93ccb0e4417c607b132dcffc5c4f
GOH23 <padadamlpd@gmail.com>
2023-03-17 23:27:07 +0300

    Changed3

--- models/User.js
+++ models/User.js
@@ -23,7 +23,7 @@ const UserSchema = new mongoose.Schema({
     },
     isActivated: {type: Boolean,default: false},
     isPrivacy: {type: Boolean,default: false},
-    forCheckProfile: {type: mongoose.Types.ObjectId,auto: true,required: true},
+    forCheckProfile: {type: mongoose.Types.ObjectId,default: function () { return new mongoose.Types.ObjectId}},
     activationLink: {type: String},
     optionsofanime: [{
         status: {
