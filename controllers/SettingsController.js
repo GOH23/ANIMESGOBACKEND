8cdf7d53afd60a85318506894d51fd2536b774fb
GOH23 <padadamlpd@gmail.com>
2023-03-19 15:00:14 +0300

    Changed3

--- controllers/SettingsController.js
+++ controllers/SettingsController.js
@@ -2,24 +2,46 @@ import UserSchema from '../models/User.js'
 export const SetSetting = async (req, res) => {
     try {
         const id =req.userId
-        const user = await UserSchema.findByIdAndUpdate({
-            _id: id
-        },{
-            avatarUrl: req.body.avatarUrl,
-            isPrivacy: req.body.isPrivacy,
-            profileDesc: req.body.descriptionProfile
-        });
-        if (!user) {
-            return res.status(404).json({
-                message: 'Не сохранено'
+        if(req.body.avatarUrl==''){
+            const user = await UserSchema.findByIdAndUpdate({
+                _id: id
+            },{
+                isPrivacy: req.body.isPrivacy,
+                profileDesc: req.body.descriptionProfile
             });
+            if (!user) {
+                return res.status(404).json({
+                    message: 'Не сохранено'
+                });
+            }
+            else{
+                user.save()
+                return res.json({
+                    message: 'Сохранено'
+                })
+            }
         }
         else{
-            user.save()
-            return res.json({
-                message: 'Сохранено'
-            })
+            const user = await UserSchema.findByIdAndUpdate({
+                _id: id
+            },{
+                avatarUrl: req.body.avatarUrl,
+                isPrivacy: req.body.isPrivacy,
+                profileDesc: req.body.descriptionProfile
+            });
+            if (!user) {
+                return res.status(404).json({
+                    message: 'Не сохранено'
+                });
+            }
+            else{
+                user.save()
+                return res.json({
+                    message: 'Сохранено'
+                })
+            }
         }
+
     } catch (err) {
         console.log(err)
         res.status(500).json({
